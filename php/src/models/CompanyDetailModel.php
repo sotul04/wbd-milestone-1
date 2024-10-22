e13a62cb1f4d2abd67ffa051c80b285db3568e8d
dianatrihy <dianatrihy@gmail.com>
2024-10-22 09:16:49 +0700

    add company profil page

--- php/src/models/CompanyDetailModel.php
+++ php/src/models/CompanyDetailModel.php
@@ -8,7 +8,12 @@ class CompanyDetailModel {
     }
 
     public function getCompanyByUserId($userId) {
-        $this->db->query("SELECT * FROM company_details WHERE user_id = :userId");
+        $this->db->query("
+            SELECT u.nama, u.email, c.lokasi, c.about
+            FROM company_details AS c
+            JOIN users AS u ON c.user_id = u.user_id
+            WHERE c.user_id = :userId
+        ");
         $this->db->bind(':userId', $userId);
         return $this->db->single();
     }
@@ -16,6 +21,14 @@ class CompanyDetailModel {
     public function createCompanyDetail($userId, $lokasi, $about) {
         $this->db->query("INSERT INTO company_details (user_id, lokasi, about) VALUES (:user_id, :lokasi, :about)");
         $this->db->bind(':user_id', $userId);
+        $this->db->bind(':lokasi', $lokasi);
+        $this->db->bind(':about', $about);
+        return $this->db->execute();
+    }
+
+    public function updateCompanyDetail($userId, $lokasi, $about) {
+        $this->db->query("UPDATE company_details SET lokasi = :lokasi, about = :about WHERE user_id = :userId");
+        $this->db->bind(':userId', $userId);
         $this->db->bind(':lokasi', $lokasi);
         $this->db->bind(':about', $about);
         return $this->db->execute();
